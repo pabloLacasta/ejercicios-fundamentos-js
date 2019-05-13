0c20f8982157c4a2934e7238d000a5420206b53d
Pablo Lacasta <bapalnas@gmail.com>
2019-05-13 17:26:26 +0200

    ex9 done

--- 1-bucles/9-array-bidimensional.js
+++ 1-bucles/9-array-bidimensional.js
@@ -12,3 +12,14 @@ let matriz = [
     [],
     []
 ]
+var num1 = 0;
+var num2 = 0;
+for (var i = 0; i < 3; i++){
+    num1 = i;
+    for(var j = 0; j < 3; j++){
+        num2 = j;
+        matriz[i][j] = num1.toString()+ num2.toString();
+    }
+}
+
+console.log(matriz);
