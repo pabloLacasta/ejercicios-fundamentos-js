bc5ca734349d27c8094506036038e9003457e91d
Pablo Lacasta <bapalnas@gmail.com>
2019-05-14 10:56:13 +0200

    ej6 functions medium done

--- 6-functions-medium/ej6.js
+++ 6-functions-medium/ej6.js
@@ -13,7 +13,17 @@
 let transformaArrayEnOtraConSusLongitudes = (arrayDeStrings) => {
     let arrayDeLongitudes = [];
     // Aquí tu código.  Desde aquí:
+    if (typeof arrayDeStrings === 'object' && arrayDeStrings.length >= 0){
+        for(let elemento of arrayDeStrings){
+        arrayDeLongitudes.push(elemento.length);
+        }
+        return arrayDeLongitudes;
+    }
 
+    else {
+        return 'Debo ser ejecutada con un array';
+    }
+    
     // Hasta aquí.
     return arrayDeLongitudes
 }
